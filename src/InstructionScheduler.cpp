90c6af784683464f545cc15c1bec9c2fadc8cb54
Jeff Lieu <lieumychuong@gmail.com>
2020-04-20 21:36:31 +1000

    20200420

--- src/InstructionScheduler.cpp
+++ src/InstructionScheduler.cpp
@@ -38,7 +38,7 @@ void InstructionScheduler::schedule(Function * irFunction)
   LOG_S(IS_DBG) << "Assigning birth time to global variables and arguments \n";      
     
   //Assign birth time for Global Variable 
-  for(auto gv_i = M.global_begin(), gv_last = M.global_end(); gv_i != gv_last; ++gv_i)
+  for(auto gv_i = M.global_begin(), gv_end = M.global_end(); gv_i != gv_end; ++gv_i)
     {
       LOG_S(IS_DBG) << *gv_i << "\n" ;
       auto ret = CDI_h->addValueInfo(&*gv_i);
@@ -46,12 +46,30 @@ void InstructionScheduler::schedule(Function * irFunction)
     }
 
     //Assign valid_time for Arguments:   
-  for(auto arg_i = F.arg_begin(), arg_last = F.arg_end(); arg_i != arg_last; arg_i ++ )
+  for(auto arg_i = F.arg_begin(), arg_end = F.arg_end(); arg_i != arg_end; arg_i ++ )
     {
       LOG_S(IS_DBG) << *arg_i << "\n";
       auto ret = CDI_h->addValueInfo(&*arg_i);
       ret.first->second.setBirthTime(&entryBlock, 0.0);     
     }
+
+  for(auto bb_i = F.begin(), bb_end = F.end(); bb_i != bb_end ; ++bb_i)
+    {
+      LOG_S(IS_DBG) << " Block : " << bb_i->getName() << "\n";
+      std::list<Instruction * > instructions;                      
+      //Collecting all instructions in the basic block 
+      for(auto ins_i = bb_i->begin(), ins_end = bb_i->end(); ins_i != ins_end; ++ ins_i)
+      {
+        instructions.push_back(&*ins_i);
+        
+        //Update name
+        if (ins_i->getName().empty() && !ins_i->getType()->isVoidTy()) {
+          ins_i->setName(Twine('s') + Twine::utohexstr(reinterpret_cast<intptr_t>(&*ins_i)));
+        }        
+        
+        LOG_S(IS_DBG) << "Instruction: " << *ins_i << " ID: " << &*ins_i;              
+      }
+   }
 
   LOG(INFO, "... done ");
 }
